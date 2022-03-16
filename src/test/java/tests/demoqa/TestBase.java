3fb1afa999d02d2c7fda1e2d38a7d35b63eab570
amstell27 <sergei_i27@mail.ru>
2022-03-16 17:59:23 +0700

    added Selenide listener

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -1,6 +1,8 @@
 package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -9,7 +11,7 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
-
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
 
