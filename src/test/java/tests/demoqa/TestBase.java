30d764b8cd614ab07637f4688fd4be2c460e3b20
amstell27 <sergei_i27@mail.ru>
2022-03-16 17:53:05 +0700

    change selenoid support

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -14,8 +14,8 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", "true");
-        capabilities.setCapability("enableVideo", "true");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
         Configuration.remote =  "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
