e6db0373feafeb15f262e0d5b06df90d1c3be619
amstell27 <sergei_i27@mail.ru>
2022-03-18 12:46:53 +0700

    added tags

--- src/test/java/tests/demoqa/RegistrationTests.java
+++ src/test/java/tests/demoqa/RegistrationTests.java
@@ -11,7 +11,6 @@ import static com.codeborne.selenide.Selenide.open;
 public class RegistrationTests extends TestBase {
 
     @Test
-    @Disabled
     void fillFormTest() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
--- src/test/java/tests/properties/SystemPropertiesTests.java
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -1,45 +1,41 @@
 package tests.properties;
 
-import com.google.gson.internal.bind.util.ISO8601Utils;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 @Tag("properties")
 public class SystemPropertiesTests {
-
-
     @Test
-    void someTest1(){
+    void someTest1() {
         String browser = System.getProperty("browser");
-        System.out.println(browser);  //null
+        System.out.println(browser); // null
     }
 
     @Test
-    void someTest2(){
+    void someTest2() {
         System.setProperty("some_browser", "safari");
         String browser = System.getProperty("some_browser");
-        System.out.println(browser);  //safari
+        System.out.println(browser); // safari
     }
 
     @Test
-    @Tag("smoke")
-    void someTest3(){
+    void someTest3() {
         String browser = System.getProperty("browser", "opera");
-        System.out.println(browser);  //opera
+        System.out.println(browser);
+        // gradle clean test                          // opera
+        // gradle clean test -Dbrowser=mozilla        // mozilla
     }
 
     @Test
-    void someTest4(){
-        System.out.println(System.getProperty("any_value"));  //null
+    void someTest4() {
+        System.out.println(System.getProperty("any_value")); // null
 
         System.setProperty("any_value", "some");
-        System.out.println(System.getProperty("any_value"));  //some
+        System.out.println(System.getProperty("any_value")); // some
     }
 
-
     @Test
-    void someTest5(){
+    void someTest5() {
         String browser = System.getProperty("browser", "chrome");
         String version = System.getProperty("version", "91");
         String browserSize = System.getProperty("browserSize", "300x300");
@@ -48,21 +44,19 @@ public class SystemPropertiesTests {
         System.out.println(version);
         System.out.println(browserSize);
 
-        //gradle clean test
-//        chrome
-//        91
-//        300x300
-
-        //gradle clean test -Dbrowser=mozilla -Dversion=80 -DbrowserSize=600x600
-//        mozilla
-//        80
-//        600x600
-
+        /* gradle clean test
+            chrome
+            91
+            300x300 */
+        /* gradle clean test -Dbrowser=mozilla -Dversion=80 -DbrowserSize=600x600
+            mozilla
+            80
+            600x600 */
     }
 
     @Test
     @Tag("smoke")
-    void someTest6(){
-        System.out.println("I want to say: " + System.getProperty("some_text"));  //some
+    void someTest6() {
+        System.out.println("I want to say: " + System.getProperty("someText"));
     }
 }
